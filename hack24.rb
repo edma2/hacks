1858bfa13cb7751d3050554824ef282d415713b9
Eugene Ma <edma2@SAC.localdomain>
2011-08-18 22:46:05 -0700

    to_f last arg

--- hack24.rb
+++ hack24.rb
@@ -1,6 +1,6 @@
 ARGV.permutation.each do |p|
   ['+','-','*','/'].repeated_permutation(3).each do |o|
-    s = "((%.0f %c %.0f) %c %.0f) %c %d" % p.zip(o).flatten.compact
+    s = "((%.0f %c %.0f) %c %.0f) %c %.0f" % p.zip(o).flatten.compact
     puts s if eval(s)==24
   end
 end
