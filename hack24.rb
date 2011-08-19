579848532533be863c11c95f39a6228cc24671a6
Eugene Ma <edma2@SAC.localdomain>
2011-08-18 22:43:56 -0700

    removed unnecessary line

--- hack24.rb
+++ hack24.rb
@@ -1,7 +1,6 @@
 ARGV.permutation.each do |p|
   ['+','-','*','/'].repeated_permutation(3).each do |o|
     s = "((%.0f %c %.0f) %c %.0f) %c %d" % p.zip(o).flatten.compact
-    p.map(&:to_f).zip(o).flatten.compact.join ' ';
     puts s if eval(s)==24
   end
 end
