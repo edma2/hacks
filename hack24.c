0f51c79a37a1bc17cf5ea8729400b3ad2c2df93c
Eugene Ma <edma2@SAC.localdomain>
2011-08-27 03:08:39 -0700

    fixed round off error

--- hack24.c
+++ hack24.c
@@ -2,27 +2,20 @@
 #include <stdlib.h>
 
 enum {OP_ADD = 0, OP_SUB, OP_MUL, OP_DIV, OP_MAX};
-
 float op(int op, float x, float y) {
-        static void *table[OP_MAX] = {
-		[OP_ADD] &&l_add,
-		[OP_MUL] &&l_mul,
-		[OP_SUB] &&l_sub,
-		[OP_DIV] &&l_div
-	};
-	goto *table[op];
-l_add: return x+y;
-l_sub: return x-y;
-l_mul: return x*y;
-l_div: return x/y;
+        switch(op) {
+                case OP_ADD: return x+y;
+                case OP_SUB: return x-y;
+                case OP_MUL: return x*y;
+                case OP_DIV: return x/y;
+        }
+        return -1;
 }
 
 int main(int argc, char *argv[]) {
-        float n[4];
+        float n[4], ans;
         char s[4] = {'+', '-', '*', '/'};
-        int o1, o2, o3;
-        int a, b, c, d;
-        int i;
+        int o1, o2, o3, a, b, c, d, i;
 
         if (argc != 5) {
                 printf("Usage: %s n1 n2 n3 n4\n", argv[0]);
@@ -44,10 +37,12 @@ int main(int argc, char *argv[]) {
                                         for (d = 0; d < 4; d++) {
                                                 if (d == a || d == b || d == c)
                                                         continue;
-                                                if (op(o3, op(o2, op(o1, n[a], n[b]), n[c]), n[d]) == 24)
-                                                        printf("((%.0f %c %.0f) %c %.0f) %c %.0f\n", n[a], s[o1], n[b], s[o2], n[c], s[o3], n[d]);
-                                                if (op(o1, op(o2, n[a], n[b]), op(o3, n[c], n[d])) == 24)
-                                                        printf("(%.0f %c %.0f) %c (%.0f %c %.0f)\n", n[a], s[o2], n[b], s[o1], n[c], s[o3], n[d]);
+                                                ans = op(o3, op(o2, op(o1, n[a], n[b]), n[c]), n[d]);
+                                                if (ans >= 23.999999 && ans <= 24.0000001)
+                                                        printf("((%.1f %c %.1f) %c %.1f) %c %.1f\n", n[a], s[o1], n[b], s[o2], n[c], s[o3], n[d]);
+                                                ans = op(o2, op(o1, n[a], n[b]), op(o3, n[c], n[d]));
+                                                if (ans >= 23.999999 && ans <= 24.0000001)
+                                                        printf("(%.1f %c %.1f) %c (%.1f %c %.1f)\n", n[a], s[o1], n[b], s[o2], n[c], s[o3], n[d]);
                                         }
                                 }
                         }
